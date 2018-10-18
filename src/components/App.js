19d346337df49b8d1a2f5133e9f47af55c44f0b1
jamal7X7 <jamal7x7@gmail.com>
2018-10-18 20:29:04 +0000

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import '../styles/App.sass'
+
+const Header = () => (
+  <header className='App-header'>
+    <h1 className='App-title'> Virtues App </h1>
+  </header>
+)
+
+const Main = (props) => (
+  <div className='game'>
+
+    
+
+  </div>
+)
+
+const Side = () => (
+  <div className='score'>
+
+      
+
+  </div>
+)
+
+
+
+class  VirtuesApp extends Component {
+  
+  state = {
+    
+  }
+
+
+
+  componentDidMount() {
+    
+  
+
+  }
+
+
+      render () {
+    return (
+      <div className='App-container'>
+
+        <Side />
+        <Main />
+       
+
+      </div>
+    )
+  }
+}
+
+const App = (props) => (
+  <div className='App'>
+    <Header />
+    < VirtuesApp />
+
+  </div>
+)
+
+export default App
+
